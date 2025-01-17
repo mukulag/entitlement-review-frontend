adeeb17acb8a54eab3beacf474415dcec11d5839
Mukul Agrawal <mukulag@gmail.com>
2025-01-18 00:12:33 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+import ReviewerDashboard from './components/ReviewerDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/reviewer" element={<ReviewerDashboard />} />
       </Routes>
     </Router>
   );
