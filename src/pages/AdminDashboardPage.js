574c3945adfc1db73b527b459b0fe23f05f9ed2b
Mukul Agrawal <mukulag@gmail.com>
2025-01-18 09:09:16 +0530

    modified:   src/pages/AdminDashboardPage.js 	modified:   src/pages/ReviewerDashboardPage.js

--- src/pages/AdminDashboardPage.js
+++ src/pages/AdminDashboardPage.js
@@ -3,14 +3,14 @@ import ApplicationForm from '../components/AdminDashboard/ApplicationForm';
 import ApplicationList from '../components/AdminDashboard/ApplicationList';
 
 
-const AdminDashboardPage = () => {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Admin Dashboard</h1>
-      <p>This is where Admin functionality will go.</p>
-    </div>
-  );
-};
+//const AdminDashboardPage = () => {
+//  return (
+//    <div style={{ padding: '20px' }}>
+//      <h1>Admin Dashboard</h1>
+//      <p>This is where Admin functionality will go.</p>
+//   </div>
+//  );
+//};
 
 const AdminDashboardPage = () => {
   return (
--- src/pages/ReviewerDashboardPage.js
+++ src/pages/ReviewerDashboardPage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PendingTasks from '../components/ReviewerDashboard/PendingTasks';
 import HistoricalApprovals from '../components/ReviewerDashboard/HistoricalApprovals';
 
-const ReviewerDashboardPage = () => {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Reviewer Dashboard</h1>
-      <p>This is where Reviewer functionality will go.</p>
-    </div>
-  );
-};
+//const ReviewerDashboardPage = () => {
+// return (
+//    <div style={{ padding: '20px' }}>
+//      <h1>Reviewer Dashboard</h1>
+//      <p>This is where Reviewer functionality will go.</p>
+//    </div>
+//  );
+//};
 
 const ReviewerDashboardPage = () => {
   return (
